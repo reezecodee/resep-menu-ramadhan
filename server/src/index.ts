85cea80258ede6f5ea86ab3ce31eea2c2ed0359c
Reeze <azfaalharits25@gmail.com>
2025-03-09 16:25:21 +0700

    feat: add configs and models for mongodb

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,8 @@
 import express, { Application } from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
-import mongoose from 'mongoose';
 import routes from './routes';
+import './config/database';
 
 dotenv.config();
 
@@ -12,11 +12,6 @@ app.use(express.json());
 app.use('/api/v1', routes);
 
 const PORT = process.env.PORT;
-
-mongoose
-  .connect(process.env.MONGO_URI as string)
-  .then(() => console.log('MongoDB Connected'))
-  .catch((err) => console.error(err));
 
 app.listen(PORT, (): void => {
   console.log(`Server is running on http://localhost:${PORT}`);
